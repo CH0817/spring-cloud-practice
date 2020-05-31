e80bb7191d738b58131cddae3136d78187ffbe82
Rex Yu <yu.chenhang@gmail.com>
2020-05-31 16:16:48 +0800

    Ribbon add Hystrix 熔斷器

--- spring-cloud-practice-ribbon/src/main/java/com/rex/spring/cloud/practice/service/SayHiService.java
+++ spring-cloud-practice-ribbon/src/main/java/com/rex/spring/cloud/practice/service/SayHiService.java
@@ -1,5 +1,6 @@
 package com.rex.spring.cloud.practice.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +15,14 @@ public class SayHiService {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "sayHiError")
     public String sayHi(String name) {
         return restTemplate.getForObject("http://spring-cloud-practice-simple-service/simple/hi?name=" + name, String.class);
+    }
+
+    private String sayHiError(String name) {
+        // 當所有的 spring-cloud-practice-simple-service 都無法請求時就進入此 method
+        return String.format("Ribbon sayHi() with %s has error!", name);
     }
 
 }
