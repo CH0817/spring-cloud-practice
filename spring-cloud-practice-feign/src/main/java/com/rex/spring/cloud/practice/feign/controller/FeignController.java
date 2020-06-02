d8a92690869f8add6c0ff3b01076cd6e10661a97
Rex Yu <yu.chenhang@gmail.com>
2020-06-02 10:14:18 +0800

    init spring cloud config

--- spring-cloud-practice-feign/src/main/java/com/rex/spring/cloud/practice/feign/controller/FeignController.java
+++ spring-cloud-practice-feign/src/main/java/com/rex/spring/cloud/practice/feign/controller/FeignController.java
@@ -2,23 +2,36 @@ package com.rex.spring.cloud.practice.feign.controller;
 
 import com.rex.spring.cloud.practice.feign.service.SayHiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/feign")
 public class FeignController {
 
+    private Environment environment;
     private SayHiService sayHiService;
+    @Value("${my.identify}")
+    private String identify;
 
     @Autowired
-    public FeignController(SayHiService sayHiService) {
+    public FeignController(SayHiService sayHiService, Environment environment) {
         this.sayHiService = sayHiService;
+        this.environment = environment;
     }
 
     @GetMapping("/hi")
     public String sayHi(String name) {
+        System.out.println("======================");
+        Stream.of(environment.getActiveProfiles()).forEach(System.out::println);
+        System.out.println("======================");
+        System.out.println(identify);
+        System.out.println("======================");
         return sayHiService.sayHi(name);
     }
 
